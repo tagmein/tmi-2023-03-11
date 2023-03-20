31e55800d43eec7f6ddebaad3007535c0e11fe3a
Nate Ferrero <nate@flatfile.io>
2023-03-20 14:25:06 -0400

    security

--- src/lib/routes/default.js
+++ src/lib/routes/default.js
@@ -4,5 +4,10 @@ module.exports = async function ({ modules, requestPath, rootPath }) {
  if (requestPath === '/') {
   return replyWithFile(modules.path.join(rootPath, 'index.html'))
  }
+ if (requestPath.startsWith('/data') && requestPath.length < 45) {
+  return {
+   statusCode: 404
+  }
+ }
  return replyWithFile(modules.path.join(rootPath, decodeURIComponent(requestPath)))
 }
